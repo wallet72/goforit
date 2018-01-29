e755e5dd22bf707a80fcfc9aed828d409a86fbc9
wholder <william.holder@qantas.com.au>
2018-01-29 13:47:53 +1100

    added unit tests

--- hello-world_test.go
+++ hello-world_test.go
@@ -8,10 +8,18 @@ import (
 func Testconnect (t *testing.T){
 
 	http.HandleFunc("/",helloworld)
-	if err := http.ListenAndServe(":3333",nil); err != nil {
+	if fail := http.ListenAndServe(":3333",nil); fail != nil {
 
 		t.Errorf("could not open web server",)
 
 	}
+
+	body, err := http.Get("127.0.0.1:3333")
+	if err != nil {
+		t.Errorf("Something wrong with the response from the server")
+	}
+
+	defer body.Body.Close()
+
 
 }
