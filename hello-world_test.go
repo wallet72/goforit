a068cfa76b87de815857c132451c163448084195
wholder <william.holder@qantas.com.au>
2018-01-31 08:07:48 +1100

    debugging

--- hello-world_test.go
+++ hello-world_test.go
@@ -6,21 +6,28 @@ import (
 	"fmt"
 )
 
-func Testconnect (t *testing.T){
+func Testconnect (test *testing.T){
 
-	fmt.Printf("Starting tests...\n\n\n")
+	test.Log("Starting tests...\n\n\n")
+	fmt.Printf("Starting testes...\n\n")
 
 	http.HandleFunc("/",helloworld)
 	if fail := http.ListenAndServe(":3333",nil); fail != nil {
 
-		t.Errorf("could not open web server",)
+		test.Errorf("could not open web server",)
+		fmt.Printf("Could not open web server")
 
 	}
+
+	fmt.Printf("Web server started...\n\n")
+
+	//defer body.Body.Close()
 
 	body, err := http.Get("127.0.0.1:3333")
 	if err != nil {
-		t.Errorf("Something wrong with the response from the server")
+		test.Errorf("Something wrong with the response from the server")
 	}
+	fmt.Printf("Response received ok")
 
 	defer body.Body.Close()
 
