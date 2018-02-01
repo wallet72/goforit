195beab5aa08534f80ddc2c321e86bb1fa3a38c9
holderb <wallet72@yahoo.com.au>
2018-02-01 14:37:15 +1100

    debug

--- hello-world_test.go
+++ hello-world_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"net/http"
 	"fmt"
-	"./hello-world"
+	//"./hello-world"
 	"log"
 )
 
@@ -13,32 +13,30 @@ func Testconnect (test *testing.T) {
 	test.Log("Starting tests...\n\n\n")
 	fmt.Printf("Starting testes...\n\n")
 
-	http.HandleFunc("/", helloworld)
+	http.HandleFunc("/", webserver)
 	if fail := http.ListenAndServe(":3333", nil); fail != nil {
 
 		test.Errorf("could not open web server", )
 		fmt.Printf("Could not open web server", )
 
 	}
-}
-
-func webtest(handle *testing.T){
 
 	fmt.Printf("Web server started...\n\n")
 
-	webresponse, error := http.Get("127.0.0.1:3333")
+	webresponse, badness := http.Get("127.0.0.1:3333")
 
-	if error != nil {
+	if badness != nil {
 
-		log.Fatal("error: %s", error)
+		fmt.Printf("Error response %s", badness)
+		log.Fatal(badness)
 
 	}
 
-	if webresponse != "hello world"{
+	/*if webresponse != "hello world"{
 
-		log.Fatal("invalid response received: %S", webresponse)
-	}
-
+		log.Fatal("invalid response received: $S", webresponse)
+	}*/
+	fmt.Printf("Response received: %s\n\n", webresponse)
 	fmt.Printf("testing passed ok",)
 
 }
