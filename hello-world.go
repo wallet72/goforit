8eef531e6f425b8004c248ff953c853e5c1ea39c
holderb <wallet72@yahoo.com.au>
2018-02-01 21:28:33 +1100

    comments added

--- hello-world.go
+++ hello-world.go
@@ -2,13 +2,15 @@ package main
 
 import "net/http"
 
-
+// This is a really simple way to start a brain dead web server
+// Thanks to the net/http module.
 func webserver(web http.ResponseWriter, req *http.Request){
 	web.WriteHeader(http.StatusOK)
 	web.Write([]byte("Hello World"))
 
 }
 
+// So listen and respond with the message...
 func main(){
 	http.HandleFunc("/",webserver)
 	http.ListenAndServe(":3000",nil)
--- hello-world_test.go
+++ hello-world_test.go
@@ -7,8 +7,12 @@ import (
 	"net/http/httptest"
 )
 
+// Define the testing routine to use the brain dead listener
+// We are testing that the status is OK and that the
+// message body is what we expected
 func TestHelloworld (rtest *testing.T){
 
+	//Get it...
 	rtarget,rerror := http.NewRequest("GET","/",nil)
 	if rerror != nil {
 		fmt.Printf("Prep work failed: %s,%s", rtarget,rerror)
@@ -21,10 +25,12 @@ func TestHelloworld (rtest *testing.T){
 
 	status := incoming.Code;
 
+	//test the status
 	if status != http.StatusOK {
 		rtest.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
+	//test the message
 	if incoming.Body.String() != "Hello World" {
 		rtest.Errorf("Message body was not right: %v", incoming.Body.String())
 	}
